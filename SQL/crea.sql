fd12836051cf52409b1d9e066c0eae773bcbc886
gael.simon.28 <gael.simon.28@gmail.com>
2024-01-08 18:27:06 +0100

    debut du sql

---
+++ SQL/crea.sql
@@ -0,0 +1,14 @@
+create table MESSAGE(
+    idMessage int(4),
+    contenu varchar(400),
+    nombreLike int(32),
+    date Datetime;
+)
+
+create table CLIENT(
+    nomClient varchar(40);
+)
+
+create table ABONNEMENT(
+    
+)
